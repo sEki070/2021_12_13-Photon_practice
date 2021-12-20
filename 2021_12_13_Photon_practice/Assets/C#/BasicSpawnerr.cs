d0bed85ad83c68fb65710201e4a5634146ce02bc
seki070 <89112588+sEki070@users.noreply.github.com>
2021-12-20 11:58:56 +0800

    Update 連線與生成

--- 2021_12_13_Photon_practice/Assets/C#/BasicSpawnerr.cs
+++ 2021_12_13_Photon_practice/Assets/C#/BasicSpawnerr.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 using Fusion;           //引用Fusion命名空間
 using Fusion.Sockets;
 using System.Collections.Generic;
@@ -9,19 +10,25 @@ using System;
 /// <summary>
 /// 連線基底生成器
 /// </summary>
-public class BasicSpawnerr : MonoBehaviour,INetworkRunnerCallbacks
+public class BasicSpawnerr : MonoBehaviour, INetworkRunnerCallbacks
 {
     #region 欄位
     [Header("創見與加入房間欄位")]
     public InputField inputFieldCreateRoom;
     public InputField inpubtFieldJoinRoom;
     [Header("玩家控制物件")]
-    public GameObject goPlayer;
+    public NetworkPrefabRef goPlayer;
+    [Header("畫布連線")]
+    public GameObject goCanvas;
 
     /// <summary>
     /// 玩家輸入房間名稱
     /// </summary>
     private string roomNameInput;
+    /// <summary>
+    /// 連線執行器
+    /// </summary>
+    private NetworkRunner runner;
     #endregion
 
     #region 方法
@@ -32,89 +39,125 @@ public class BasicSpawnerr : MonoBehaviour,INetworkRunnerCallbacks
     {
         roomNameInput = inputFieldCreateRoom.text;
         print("創建房間: " + roomNameInput);
+        StartGame(GameMode.Host); //加入房間用Host
     }
 
+    /// <summary>
+    /// 按鈕點擊呼叫: 加入房間
+    /// </summary>
     public void BtnJoinRoom()
     {
         roomNameInput = inpubtFieldJoinRoom.text;
         print("加入房間: " + roomNameInput);
+        StartGame(GameMode.Client); //加入房間用Client
+    }
+
+    //async 非同步處理: 執行系統時處理連線
+    /// <summary>
+    /// 開始連線遊戲
+    /// </summary>
+    /// <param name="mode">連線模式: 主機、客戶</param>
+            //同步
+    private async void StartGame(GameMode mode)
+    {
+        print("<color=yellow>開始連線 </color>");
+
+        runner = gameObject.AddComponent<NetworkRunner>(); //連線執行器  =添加元件<連線執行器>
+        runner.ProvideInput = true;                        //連線執行器.是否提供輸入 = 是
+
+        //等待連線:遊戲連線模式、房間名稱、連線後的場景、場景管理器
+        await runner.StartGame(new StartGameArgs()
+        {
+            GameMode = mode,
+            SessionName = roomNameInput,
+            Scene = SceneManager.GetActiveScene().buildIndex,
+            SceneObjectProvider = gameObject.AddComponent<NetworkSceneManagerDefault>()
+        });
+        print("<color=yellow>連線完成 </color>");
+        goCanvas.SetActive(false);
     }
     #endregion
 
     #region Fusion 回呼函式區域
     public void OnConnectedToServer(NetworkRunner runner)
     {
-        
+
     }
 
     public void OnConnectFailed(NetworkRunner runner, NetAddress remoteAddress, NetConnectFailedReason reason)
     {
-      
+
     }
 
     public void OnConnectRequest(NetworkRunner runner, NetworkRunnerCallbackArgs.ConnectRequest request, byte[] token)
     {
-      
+
     }
 
     public void OnCustomAuthenticationResponse(NetworkRunner runner, Dictionary<string, object> data)
     {
-        
+
     }
 
     public void OnDisconnectedFromServer(NetworkRunner runner)
     {
-       
+
     }
 
     public void OnInput(NetworkRunner runner, NetworkInput input)
     {
-       
+
     }
 
     public void OnInputMissing(NetworkRunner runner, PlayerRef player, NetworkInput input)
     {
-    
+
     }
 
+    /// <summary>
+    /// 當玩家成功加入房間後
+    /// </summary>
+    /// <param name="runner">連線執行器</param>
+    /// <param name="player">玩家資訊</param>
     public void OnPlayerJoined(NetworkRunner runner, PlayerRef player)
     {
-    
+        //連線執行,生成(物件、座標、角度、玩家資訊)
+        runner.Spawn(goPlayer, new Vector3(-5, 0, -10), Quaternion.identity, player);
     }
 
     public void OnPlayerLeft(NetworkRunner runner, PlayerRef player)
     {
-     
+
     }
 
     public void OnReliableDataReceived(NetworkRunner runner, PlayerRef player, ArraySegment<byte> data)
     {
-     
+
     }
 
     public void OnSceneLoadDone(NetworkRunner runner)
     {
-      
+
     }
 
     public void OnSceneLoadStart(NetworkRunner runner)
     {
-    
+
     }
 
     public void OnSessionListUpdated(NetworkRunner runner, List<SessionInfo> sessionList)
     {
-    
+
     }
 
     public void OnShutdown(NetworkRunner runner, ShutdownReason shutdownReason)
     {
-     
+
     }
 
     public void OnUserSimulationMessage(NetworkRunner runner, SimulationMessagePtr message)
     {
-     
+
     }
     #endregion
 }
